b4f0774b9001c7c3d7dcd133700595902ffc1e46
Vikram Ramesh <vikram132@gmail.com>
2017-04-15 20:34:20 -0700

    Added bootstrap

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.set('title', 'Youtube to Spotify')
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'bower_components')))
 
 app.get('/', function(req, res) {
   res.render('index', {
     message: 'hello world!',
-    smallMessage: 'this is small'
+    another: 'another message'
   })
 })
 
