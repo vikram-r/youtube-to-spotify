cfc2835c51675e25d4b8205791f79b163bc34f56
Vikram Ramesh <vikram132@gmail.com>
2017-04-15 19:39:38 -0700

    Added ejs for view rendering

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('title', 'Youtube to Spotify')
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-  res.send("hello world!")
+  res.render('index', {
+    message: 'hello world!',
+    smallMessage: 'this is small'
+  })
 })
 
 app.listen(3000, function() {
