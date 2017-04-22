0dc58b601967342bd9cd845b797abf8bcc47ddc5
Vikram Ramesh <vikram132@gmail.com>
2017-04-22 16:17:33 -0700

    Render youtube playlist results onto page in simple list

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,15 +1,33 @@
 $(function(){
 
+  var $container = $('#youtube-list-container');
+
+  var $playlistUrlSpinner = $('#playlist-url-spinner');
+
+  Handlebars.registerPartial({
+    "youtube-list-item": $('#youtube-list-item-template').html()
+  });
+
+  var youtubeListTemplate = Handlebars.compile($('#youtube-list-template').html());
+
+  function renderYoutubeList(context) {
+    $container.html(youtubeListTemplate(context));
+  }
+
   // initial form submit
   $('#playlist-url-form').submit(function(e) {
+    $playlistUrlSpinner.removeClass('hidden');
     $.get('/youtube/playlist', {
       playlistUrl: encodeURIComponent($('#playlist-url-input').val())
     }).done(function(results) {
       console.log("success!");
-      // render some template with data
       console.log(JSON.stringify(results));
+      // render the list
+      renderYoutubeList(results);
+      $playlistUrlSpinner.addClass('hidden');
     }).fail(function() {
       console.log("error!");
+      $playlistUrlSpinner.addClass('hidden');
       // error handling
     });
 
