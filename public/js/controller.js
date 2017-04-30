7699f358472035657abd57d322840bed065189a6
Vikram Ramesh <vikram132@gmail.com>
2017-04-30 00:40:19 -0700

    Render spotify search results for each youtube video

--- public/js/controller.js
+++ public/js/controller.js
@@ -10,7 +10,18 @@ $(function(){
 
   Handlebars.registerHelper('idToYoutubeLink', (id) => {
     return "https://www.youtube.com/watch?v=" + id;
-  })
+  });
+
+  // todo this is a bit hacky, and breaks the concept of "logic-less" templating
+  Handlebars.registerHelper('rowStatusCss', (num) => {
+    if (!num) {
+      return "danger";
+    } else if (num === 1) {
+      return "success";
+    } else {
+      return "warning";
+    }
+  });
 
   var playlistListTemplate = Handlebars.compile($('#playlist-list-template').html());
 
@@ -24,10 +35,9 @@ $(function(){
     $.get('/youtube/playlist', {
       playlistUrl: encodeURIComponent($('#playlist-url-input').val())
     }).done(function(results) {
-      console.log("success!");
       console.log(JSON.stringify(results));
       // render the list
-      renderPlaylistList(results.map(r => r.youtube));
+      renderPlaylistList(results);
       $playlistUrlSpinner.addClass('hidden');
     }).fail(function() {
       console.log("error!");
