ea332623214c13f10b6ec853ecd9d99376a62af6
Vikram Ramesh <vikram132@gmail.com>
2017-05-29 16:26:39 -0700

    Added simple oauth2 login flow for spotify

--- libs/spotify-api-helper.js
+++ libs/spotify-api-helper.js
@@ -1,5 +1,12 @@
+const UUID = require('uuid')
 var SpotifyApi = require('spotify-web-api-node')
-var spotify = new SpotifyApi()
+var credentials = {
+  clientId : process.env.SPOTIFY_CLIENT_ID,
+  clientSecret : process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri : process.env.SPOTIFY_REDIRECT_URI
+}
+
+var spotify = new SpotifyApi(credentials)
 
 function bestMatches(items) {
   // todo filter down list, and potentially look up more search terms
@@ -8,16 +15,32 @@ function bestMatches(items) {
 
 var self = module.exports = {
   search: function search(query) {
-      return spotify.searchTracks(query)
-        .then(response => {
-          return response.body.tracks.items.map(i => {
-            return {
-              name: i.name,
-              previewUrl: i.preview_url
-            }
-          })
+    return spotify.searchTracks(query)
+      .then(response => {
+        return response.body.tracks.items.map(i => {
+          return {
+            name: i.name,
+            previewUrl: i.preview_url
+          }
         })
-        .then(bestMatches)
-        .catch(e => console.log("error: " + e))
+      })
+      .then(bestMatches)
+      .catch(e => console.log("error: " + e))
+  },
+
+  getAuthorizationUrl: function getAuthorizationUrl() {
+    var scopes = ["playlist-modify-public"]
+    return spotify.createAuthorizeURL(scopes, UUID.v4())
+  },
+
+  login: function login(code) {
+    return spotify.authorizationCodeGrant(code)
+      .then(response => {
+        spotify.setAccessToken(response.body['access_token'])
+        spotify.setRefreshToken(response.body['refresh_token'])
+
+        return spotify.getAccessToken()
+      })
+      .catch(e => console.log("error: " + e))
   }
 }
--- server.js
+++ server.js
@@ -3,17 +3,33 @@ var path = require('path')
 var googleApi = require('./libs/google-api-helper')
 var spotifyApi = require('./libs/spotify-api-helper')
 var app = express()
+var session = require('express-session');
 var Promise = require('bluebird')
+const UUID = require('uuid')
 
 app.set('title', 'Youtube to Spotify')
 app.set('view engine', 'ejs')
+// static content directory
 app.use(express.static(path.join(__dirname, 'public')))
+// session variable setup
+app.use(session({
+  // store: new RedisStore, // todo use redis, this is not production safe!
+  secret: UUID.v4(),
+  saveUninitialized: true,
+  resave: true
+}));
 
 app.get('/', (req, res) => {
-  res.render('index', {
+  var model = {
     message: 'hello world!',
     another: 'another message'
-  })
+  }
+  if (req.session.spotify_token) {
+    // todo get logged in name and save to model
+  } else {
+    model['spotify_authorization_url'] = spotifyApi.getAuthorizationUrl()
+  }
+  res.render('index', model)
 })
 
 app.get('/youtube/playlist', (req, res) => {
@@ -39,13 +55,24 @@ app.get('/youtube/playlist', (req, res) => {
 })
 
 app.put('/spotify/playlist/:playlistUrl', (req, res) => {
-  var url = req.query["playlistUrl"]
-  var playlistData = req.param["playlistData"]
+  var url = req.params['playlistUrl']
+  var playlistData = req.query['playlistData']
 
   // update playlist on spotify, and return status
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify({ placeholder: "some_response" }));
   res.end()
+})
+
+// oauth2 redirect endpoint
+app.get('/oauth2/callback/:apiName', (req, res) => {
+  var code = req.query["code"]
+  spotifyApi.login(code)
+    .then(token => {
+      req.session['spotify_token'] = token
+      res.redirect('/');
+    })
+    .catch(e => console.log(e))
 })
 
 app.listen(3000, () => {
